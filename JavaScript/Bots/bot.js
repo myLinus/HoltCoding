e3d458927297a45db8424e078fb9e095b7027f88
jonas <jonas.refby@gmail.com>
2020-09-22 22:19:57 +0200

    :))))

--- JavaScript/Bots/bot.js
+++ JavaScript/Bots/bot.js
@@ -1,6 +1,8 @@
 const Discord = require('discord.js');
 const { prefix, token } = require('./config.json');
 const client = new Discord.Client();
+const { promisify } = require('util')
+const sleep = promisify(setTimeout)
 
 client.once('ready', () => {
     console.log('Ready!');
@@ -8,11 +10,17 @@ client.once('ready', () => {
 
 //besked lytter /s 
 client.on('message', message => {
-
+    if (message.author.bot) return;
+    
     // refys shit show af en if-statement
     if (message.content === `${prefix}refy_is_a_godlike_developer` ) {
         message.channel.send('I swear to Allah. Only people bored enough to read the source code should know this..')
-        message.channel.send(`But .. I guess you are just a really bored person, ${message.author.username}`)
+        sleep(2000)
+        message.channel.send(`But .. I guess you are just a really bored person, ${message.author}`)
+        sleep(1337420)
+        message.channel.send(`hey, ${message.author}. Did you really think those last two messages were it? Lol. You have awoken the beast - and the punishment will be severe!`)
+        sleep(2000)
+        message.channel.send(`${message.author} actually I will be fair. Your faith shall be decided by the king of ${message.guild.name}! `)
     }
 
     console.log(message.content);
