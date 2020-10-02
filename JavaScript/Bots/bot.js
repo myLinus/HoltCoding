e619316a0e98d71caf21c5a672c7b4604b490b8c
jonas <jonas.refby@gmail.com>
2020-10-02 19:48:25 +0200

    :)

--- JavaScript/Bots/bot.js
+++ JavaScript/Bots/bot.js
@@ -1,6 +1,9 @@
+const fs = require('fs');
 const Discord = require('discord.js');
 const { prefix, token } = require('./config.json');
 const client = new Discord.Client();
+client.commands = new Discord.Collection();
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
 const { promisify } = require('util')
 const sleep = promisify(setTimeout)
 
