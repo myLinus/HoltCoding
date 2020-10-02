4d7a7f1dda8f70de41e9dd5b86f7c5a03b8fc0ba
Iversen13 <user3782@gmail.com>
2020-10-02 19:47:48 +0200

    Updated bot.js and refy.js

---
+++ JavaScript/Bots/Commands/Refy.js
@@ -0,0 +1,10 @@
+module.exports = {
+	name: 'refy_is_a_godlike_developer',
+	description: 'refy_is_a_godlike_developer!',
+	execute(message, args) {
+        message.channel.send('I swear to Allah. Only people bored enough to read the source code should know this..')
+        message.channel.send(`But .. I guess you are just a really bored person, ${message.author}`)
+        message.channel.send(`hey, ${message.author}. Did you really think those last two messages were it? Lol. You have awoken the beast - and the punishment will be severe!`)
+        message.channel.send(`${message.author} actually I will be fair. Your faith shall be decided by the king of ${message.guild.name}! `)
+	},
+};
--- JavaScript/Bots/bot.js
+++ JavaScript/Bots/bot.js
@@ -4,8 +4,6 @@ const { prefix, token } = require('./config.json');
 const client = new Discord.Client();
 client.commands = new Discord.Collection();
 const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
-const { promisify } = require('util')
-const sleep = promisify(setTimeout)
 
 client.once('ready', () => {
     console.log('Ready!');
@@ -17,17 +15,6 @@ client.on('message', message => {
 
     const args = message.content.slice(prefix.length).trim().split(/ +/);
     const command = args.shift().toLowerCase();
-
-    // refys shit show af en if-statement
-    if (command === `refy_is_a_godlike_developer` ) {
-        message.channel.send('I swear to Allah. Only people bored enough to read the source code should know this..')
-        sleep(2000)
-        message.channel.send(`But .. I guess you are just a really bored person, ${message.author}`)
-        sleep(5000)
-        message.channel.send(`hey, ${message.author}. Did you really think those last two messages were it? Lol. You have awoken the beast - and the punishment will be severe!`)
-        sleep(2000)
-        message.channel.send(`${message.author} actually I will be fair. Your faith shall be decided by the king of ${message.guild.name}! `)
-    }
 
     console.log(message.content);
 });
